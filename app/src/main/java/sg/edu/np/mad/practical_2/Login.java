a7a557c8e9a8f01f597e06f0f11934231ca81b65
Shanice Yeong <shaniceyeong@gmail.com>
2022-05-28 02:13:27 +0800

    Firebase login

--- app/src/main/java/sg/edu/np/mad/practical_2/Login.java
+++ app/src/main/java/sg/edu/np/mad/practical_2/Login.java
@@ -1,5 +1,6 @@
 package sg.edu.np.mad.practical_2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,10 +10,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
     EditText un, pw;
     Button login;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://login-c6d7c-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +39,35 @@ public class Login extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Login.this, ListActivity.class);
-                startActivity(i);
+                final String username = un.getText().toString();
+                final String pass = pw.getText().toString();
+
+                databaseReference.child("User").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild("mad")) {
+                            final String getPW = snapshot.child("mad").child("password").getValue(String.class);
+
+                            if(getPW.equals(pass)) {
+                                Toast.makeText(Login.this, "Success", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Login.this, ListActivity.class));
+
+                            }
+                            else {
+                                Toast.makeText(Login.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(Login.this, "Wrong username", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
             }
         });
     }
